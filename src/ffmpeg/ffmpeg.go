24b796e4b461d200e3af40a4e10ffb1516d98e63
Zack Scholl <zack.scholl@gmail.com>
2020-05-15 08:00:10 -0700

    add split merge logic

--- src/ffmpeg/ffmpeg.go
+++ src/ffmpeg/ffmpeg.go
@@ -2,8 +2,11 @@ package ffmpeg
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/schollz/logger"
@@ -16,6 +19,8 @@ type AudioSegment struct {
 	End      float64
 	Duration float64
 }
+
+const SECONDSATEND = 0.05
 
 // SplitOnSilence splits any audio file based on its silence
 func SplitOnSilence(fname string, silenceDB int, silenceMinimumSeconds float64) (segments []AudioSegment, err error) {
@@ -58,7 +63,7 @@ func SplitOnSilence(fname string, silenceDB int, silenceMinimumSeconds float64) 
 	newSegments := make([]AudioSegment, len(segments))
 	i := 0
 	for _, segment := range segments {
-		if segment.Duration > 0 {
+		if segment.Duration > 0.1 {
 			newSegments[i] = segment
 			i++
 		}
@@ -72,22 +77,37 @@ func SplitOnSilence(fname string, silenceDB int, silenceMinimumSeconds float64) 
 }
 
 // Split will take AudioSegments and split them apart
-func Split(segments []AudioSegment, fnamePrefix string) (splitSegments []AudioSegment, err error) {
+func Split(segments []AudioSegment, fnamePrefix string, addsilence bool) (splitSegments []AudioSegment, err error) {
 	splitSegments = make([]AudioSegment, len(segments))
 	for i := range segments {
 		splitSegments[i] = segments[i]
 		splitSegments[i].Filename = fmt.Sprintf("%s%d.wav", fnamePrefix, i)
+		splitSegments[i].Duration += 0.1
 		var out []byte
-		cmd := fmt.Sprintf("-y -i %s -acodec copy -ss %2.8f -to %2.8f %s", segments[i].Filename, segments[i].Start, segments[i].End, splitSegments[i].Filename)
+		cmd := fmt.Sprintf("-y -i %s -acodec copy -ss %2.8f -to %2.8f 0%s", segments[i].Filename, segments[i].Start, segments[i].End, splitSegments[i].Filename)
+		if !addsilence {
+			cmd = fmt.Sprintf("-y -i %s -acodec copy -ss %2.8f -to %2.8f %s", segments[i].Filename, segments[i].Start, segments[i].End, splitSegments[i].Filename)
+		}
 		logger.Debug(cmd)
 		out, err = exec.Command("ffmpeg", strings.Fields(cmd)...).CombinedOutput()
 		if err != nil {
 			logger.Errorf("ffmpeg: %s", out)
 			return
 		}
+		if addsilence {
+			// -af 'apad=pad_dur=0.1' adds SECONDSATEND milliseconds of silence to the end
+			cmd = fmt.Sprintf("-y -i 0%s -af apad=pad_dur=%2.3f %s", splitSegments[i].Filename, SECONDSATEND, splitSegments[i].Filename)
+			logger.Debug(cmd)
+			out, err = exec.Command("ffmpeg", strings.Fields(cmd)...).CombinedOutput()
+			if err != nil {
+				logger.Errorf("ffmpeg: %s", out)
+				return
+			}
+			os.Remove(fmt.Sprintf("0%s", splitSegments[i].Filename))
+		}
 	}
 
-	// also generate the audio waveform for each
+	// also generate the audio waveform image for each
 	colors := []string{"7FFFD4", "F5F5DC"}
 	allfnames := make([]string, len(splitSegments))
 	for i := range splitSegments {
@@ -102,7 +122,7 @@ func Split(segments []AudioSegment, fnamePrefix string) (splitSegments []AudioSe
 			return
 		}
 	}
-	// generate a merged audio waveform
+	// generate a merged audio waveform image
 	cmd := fmt.Sprintf("%s +append %s-merge.png", strings.Join(allfnames, " "), fnamePrefix)
 	logger.Debug(cmd)
 	out, err := exec.Command("convert", strings.Fields(cmd)...).CombinedOutput()
@@ -111,5 +131,81 @@ func Split(segments []AudioSegment, fnamePrefix string) (splitSegments []AudioSe
 		return
 	}
 
+	return
+}
+
+// Merge takes audio segments and creates merges of at most `secondsInEachMerge` seconds
+func Merge(segments []AudioSegment, fnamePrefix string, secondsInEachMerge float64) (mergedSegments []AudioSegment, err error) {
+	fnamesToMerge := []string{}
+	currentLength := 0.0
+	mergeNum := 0
+	for _, segment := range segments {
+		if segment.Duration+currentLength > secondsInEachMerge {
+			var mergeSegment AudioSegment
+			mergeSegment, err = MergeAudioFiles(fnamesToMerge, fmt.Sprintf("%s%d.wav", fnamePrefix, mergeNum))
+			if err != nil {
+				return
+			}
+			mergedSegments = append(mergedSegments, mergeSegment)
+			currentLength = 0
+			fnamesToMerge = []string{}
+			mergeNum++
+		}
+		fnamesToMerge = append(fnamesToMerge, segment.Filename)
+		currentLength += segment.Duration
+	}
+	var mergeSegment AudioSegment
+	mergeSegment, err = MergeAudioFiles(fnamesToMerge, fmt.Sprintf("%s%d.wav", fnamePrefix, mergeNum))
+	if err != nil {
+		return
+	}
+	mergedSegments = append(mergedSegments, mergeSegment)
+
+	return
+}
+
+func MergeAudioFiles(fnames []string, outfname string) (segment AudioSegment, err error) {
+	f, err := ioutil.TempFile(os.TempDir(), "merge")
+	if err != nil {
+		return
+	}
+	// defer os.Remove(f.Name())
+
+	for _, fname := range fnames {
+		fname, err = filepath.Abs(fname)
+		if err != nil {
+			return
+		}
+		_, err = f.WriteString(fmt.Sprintf("file '%s'\n", fname))
+		if err != nil {
+			return
+		}
+	}
+	f.Close()
+
+	cmd := fmt.Sprintf("-y -f concat -safe 0 -i %s -c copy %s", f.Name(), outfname)
+	logger.Debug(cmd)
+	out, err := exec.Command("ffmpeg", strings.Fields(cmd)...).CombinedOutput()
+	logger.Debugf("ffmpeg: %s", out)
+	if err != nil {
+		err = fmt.Errorf("ffmpeg; %s", err.Error())
+		return
+	}
+	seconds, err := utils.ConvertToSeconds(utils.GetStringInBetween(string(out), "time=", " bitrate"))
+
+	segment.Duration = seconds
+	segment.End = seconds
+	segment.Filename = outfname
+
+	// create audio waveform
+	cmd = fmt.Sprintf("-i %s -o %s.png --background-color ffffff00 --waveform-color ffffff --amplitude-scale 1 --no-axis-labels --pixels-per-second 100 --height 80 --width %2.0f",
+		segment.Filename, segment.Filename, segment.Duration*100,
+	)
+	logger.Debug(cmd)
+	out, err = exec.Command("audiowaveform", strings.Fields(cmd)...).CombinedOutput()
+	if err != nil {
+		logger.Errorf("audiowaveform: %s", out)
+		return
+	}
 	return
 }
--- src/ffmpeg/ffmpeg_test.go
+++ src/ffmpeg/ffmpeg_test.go
@@ -2,21 +2,50 @@ package ffmpeg
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSplitOnSilence(t *testing.T) {
+func TestSplitAndMerge(t *testing.T) {
 	segments, err := SplitOnSilence("tests/1.aif", -18, 0.05)
 	assert.Nil(t, err)
 	for _, segment := range segments {
 		fmt.Printf("segment: %+v\n", segment)
 	}
 
-	splitSegments, err := Split(segments, "split")
+	splitSegments, err := Split(segments, "split", true)
 	assert.Nil(t, err)
 	for _, segment := range splitSegments {
 		fmt.Printf("segment: %+v\n", segment)
 	}
+
+	mergedSegments, err := Merge(splitSegments, "merge", 6)
+	assert.Nil(t, err)
+	for _, segment := range mergedSegments {
+		fmt.Printf("segment: %+v\n", segment)
+	}
+
+	// split the new merged segments on silence
+	for i, mergeSegment := range mergedSegments {
+		mergedSegmentsSegments, err := SplitOnSilence(mergeSegment.Filename, -25, 0.05)
+		assert.Nil(t, err)
+		for _, segment := range mergedSegmentsSegments {
+			fmt.Printf("segment: %+v\n", segment)
+		}
+
+		splitMergedSegments, err := Split(mergedSegmentsSegments, fmt.Sprintf("splitmerge%d", i), false)
+		assert.Nil(t, err)
+		for _, segment := range splitMergedSegments {
+			fmt.Printf("segment: %+v\n", segment)
+			os.Remove(segment.Filename)
+		}
+	}
+}
+
+func TestMerge(t *testing.T) {
+	segment, err := MergeAudioFiles([]string{"tests/1.aif", "tests/1.aif"}, "1-1.aif")
+	assert.Nil(t, err)
+	fmt.Println(segment)
 }
