3556965d9003aeafe13fc0791c5d80e960d7cb71
Zack Scholl <zack.scholl@gmail.com>
2021-08-03 09:50:24 -0700

    improve

--- main.go
+++ main.go
@@ -1,64 +1,96 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"os"
 
 	log "github.com/schollz/logger"
 	"github.com/schollz/teoperator/src/convert"
-	"github.com/schollz/teoperator/src/download"
-	"github.com/schollz/teoperator/src/ffmpeg"
-	"github.com/schollz/teoperator/src/server"
+	cli "github.com/urfave/cli/v2"
 )
 
 func main() {
-	var flagSynth, flagOut, flagDuct, flagServerName string
-	var flagDebug, flagServer, flagWorker, flagDrum bool
-	var flagPort int
-	var flagFreq float64
-	flag.BoolVar(&flagDebug, "debug", false, "debug mode")
-	flag.BoolVar(&flagDrum, "drum", false, "build drum patch")
-	flag.BoolVar(&flagServer, "serve", false, "make a server")
-	flag.BoolVar(&flagWorker, "work", false, "start a download worker")
-	flag.Float64Var(&flagFreq, "freq", 440, "base frequency when generating synth patch")
-	flag.IntVar(&flagPort, "port", 8053, "port to use")
-	flag.StringVar(&flagSynth, "synth", "", "build synth patch from file")
-	flag.StringVar(&flagOut, "out", "", "name of new patch")
-	flag.StringVar(&flagDuct, "duct", "", "name of duct")
-	flag.StringVar(&flagServerName, "server", "http://localhost:8053", "name of external ip")
-	flag.Parse()
-
-	download.Duct = flagDuct
-	download.ServerName = flagServerName
-	log.SetLevel("error")
-
-	if !ffmpeg.IsInstalled() {
-		fmt.Println("ffmpeg not installed")
-		fmt.Println("you can install it here: https://www.ffmpeg.org/download.html")
-		os.Exit(1)
+	app := &cli.App{}
+	app.UseShortOptionHandling = true
+	app.Commands = []*cli.Command{
+		{
+			Name:  "drum",
+			Usage: "create drum patch from file(s)",
+			Flags: []cli.Flag{
+				&cli.IntFlag{Name: "slices", Usage: "number of slices", Value: 0},
+				&cli.StringSliceFlag{Name: "file", Aliases: []string{"f"}, Usage: "file(s)"},
+			},
+			Action: func(c *cli.Context) error {
+				err := convert.ToDrum(c.StringSlice("file"), c.Int("slices"))
+				return err
+			},
+		},
+		{
+			Name:  "synth",
+			Usage: "create synth patch from file",
+			Flags: []cli.Flag{
+				&cli.Float64Flag{Name: "freq", Aliases: []string{"s"}, Value: 440, Usage: "base frequency"},
+				&cli.StringFlag{Name: "file", Aliases: []string{"f"}, Usage: "file"},
+			},
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
 	}
 
-	if flagDebug {
-		log.SetLevel("debug")
-	} else {
-		log.SetLevel("info")
-	}
-
-	var err error
-	if flagServer {
-		err = server.Run(flagPort, flagServerName)
-	} else if flagSynth != "" {
-		err = convert.ToSynth(flagSynth, flagFreq)
-	} else if flagDrum {
-		err = convert.ToDrum(flag.Args())
-	} else if flagWorker {
-		err = download.Work()
-	} else {
-		flag.PrintDefaults()
-	}
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Error(err)
-		os.Exit(1)
 	}
 }
+
+// func run() {
+// 	var flagSynth, flagOut, flagDuct, flagServerName string
+// 	var flagDebug, flagServer, flagWorker, flagDrum bool
+// 	var flagPort, flagSlices int
+// 	var flagFreq float64
+// 	flag.BoolVar(&flagDebug, "debug", false, "debug mode")
+// 	flag.BoolVar(&flagDrum, "drum", false, "build drum patch")
+// 	flag.BoolVar(&flagServer, "serve", false, "make a server")
+// 	flag.BoolVar(&flagWorker, "work", false, "start a download worker")
+// 	flag.Float64Var(&flagFreq, "freq", 440, "base frequency when generating synth patch")
+// 	flag.IntVar(&flagPort, "port", 8053, "port to use")
+// 	flag.IntVar(&flagSlices, "slice", 0, "if making drum patch, define number of slices")
+// 	flag.StringVar(&flagSynth, "synth", "", "build synth patch from file")
+// 	flag.StringVar(&flagOut, "out", "", "name of new patch")
+// 	flag.StringVar(&flagDuct, "duct", "", "name of duct")
+// 	flag.StringVar(&flagServerName, "server", "http://localhost:8053", "name of external ip")
+// 	flag.Parse()
+
+// 	download.Duct = flagDuct
+// 	download.ServerName = flagServerName
+// 	log.SetLevel("error")
+
+// 	if !ffmpeg.IsInstalled() {
+// 		fmt.Println("ffmpeg not installed")
+// 		fmt.Println("you can install it here: https://www.ffmpeg.org/download.html")
+// 		os.Exit(1)
+// 	}
+
+// 	if flagDebug {
+// 		log.SetLevel("debug")
+// 	} else {
+// 		log.SetLevel("info")
+// 	}
+
+// 	var err error
+// 	if flagServer {
+// 		err = server.Run(flagPort, flagServerName)
+// 	} else if flagSynth != "" {
+// 		err = convert.ToSynth(flagSynth, flagFreq)
+// 	} else if flagDrum {
+// 		err = convert.ToDrum(flag.Args())
+// 	} else if flagWorker {
+// 		err = download.Work()
+// 	} else {
+// 		flag.PrintDefaults()
+// 	}
+// 	if err != nil {
+// 		log.Error(err)
+// 		os.Exit(1)
+// 	}
+// }
--- src/convert/convert.go
+++ src/convert/convert.go
@@ -23,30 +23,44 @@ func ToSynth(fname string, baseFreq float64) (err error) {
 	return
 }
 
-func ToDrumSplice(fname string) (err error) {
+func ToDrumSplice(fname string, slices int) (err error) {
 	finalName := strings.TrimSuffix(fname, filepath.Ext(fname)) + "_patch.aif"
 	fname2, err := ffmpeg.ToMono(fname)
 	defer os.Remove(fname2)
 	if err != nil {
 		return
 	}
-	segments, err := ffmpeg.SplitOnSilence(fname2, -22, 0.2, -0.2)
-	if err != nil {
-		return
-	}
 	op1data := op1.NewDrumPatch()
-	for i, seg := range segments {
-		if i < len(op1data.End)-2 {
-			start := int64(math.Floor(math.Round(seg.Start*100) * 441 * 4058))
-			end := int64(math.Floor(math.Round(seg.End*100) * 441 * 4058))
-			if start > end {
-				continue
+	if slices == 0 {
+		segments, errSplit := ffmpeg.SplitOnSilence(fname2, -22, 0.2, -0.2)
+		if errSplit != nil {
+			err = errSplit
+			return
+		}
+		for i, seg := range segments {
+			if i < len(op1data.End)-2 {
+				start := int64(math.Floor(math.Round(seg.Start*100)*441)) * op1.SAMPLECONVERSION
+				end := int64(math.Floor(math.Round(seg.End*100)*441)) * op1.SAMPLECONVERSION
+				if start > end {
+					continue
+				}
+				if end > op1data.End[len(op1data.End)-1] {
+					continue
+				}
+				op1data.Start[i] = start
+				op1data.End[i] = end
 			}
-			if end > op1data.End[len(op1data.End)-1] {
-				continue
-			}
-			op1data.Start[i] = start
-			op1data.End[i] = end
+		}
+	} else {
+		var totalSamples int64
+		totalSamples, err = ffmpeg.NumSamples(fname2)
+		if err != nil {
+			return
+		}
+		log.Debugf("found %d samples", totalSamples)
+		for i := 0; i < slices; i++ {
+			op1data.Start[i] = int64(i) * totalSamples / int64(slices) * op1.SAMPLECONVERSION
+			op1data.End[i] = int64(i+1) * totalSamples / int64(slices) * op1.SAMPLECONVERSION
 		}
 	}
 
@@ -57,13 +71,13 @@ func ToDrumSplice(fname string) (err error) {
 	return
 }
 
-func ToDrum(fnames []string) (err error) {
+func ToDrum(fnames []string, slices int) (err error) {
 	if len(fnames) == 0 {
 		err = fmt.Errorf("no files!")
 		return
 	}
 	if len(fnames) == 1 {
-		return ToDrumSplice(fnames[0])
+		return ToDrumSplice(fnames[0], slices)
 	}
 	_, finalName := filepath.Split(fnames[0])
 	finalName = strings.TrimSuffix(finalName, filepath.Ext(fnames[0])) + "_patch.aif"
@@ -110,10 +124,9 @@ func ToDrum(fnames []string) (err error) {
 		if i == 0 {
 			drumPatch.Start[i] = 0
 		} else {
-			// 2147483646/(44100*12)
-			drumPatch.Start[i] = (sampleEnd[i-1]) * 4058
+			drumPatch.Start[i] = (sampleEnd[i-1]) * op1.SAMPLECONVERSION
 		}
-		drumPatch.End[i] = (sampleEnd[i]) * 4058
+		drumPatch.End[i] = (sampleEnd[i]) * op1.SAMPLECONVERSION
 	}
 
 	err = drumPatch.Save(fname2, finalName)
