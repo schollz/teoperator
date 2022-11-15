d3827f77e19537350cef303fba62c1d15773b010
Zack <zack.scholl@gmail.com>
2022-11-15 14:29:39 -0800

    add ToDrum2

--- src/convert/convert.go
+++ src/convert/convert.go
@@ -153,3 +153,69 @@ func ToDrum(fnames []string, slices int) (err error) {
 	}
 	return
 }
+
+
+func ToDrum2(fnames []string, slices int) (finalName string, err error) {
+	if len(fnames) == 0 {
+		err = fmt.Errorf("no files!")
+		return
+	}
+	if len(fnames) == 1 {
+		return ToDrumSplice(fnames[0], slices)
+	}
+	_, finalName = filepath.Split(fnames[0])
+	finalName = newName(finalName)
+	log.Debugf("converting %+v", fnames)
+	f, err := ioutil.TempFile(".", "concat")
+	defer os.Remove(f.Name())
+	sampleEnd := make([]int64, len(fnames))
+	fnames2 := make([]string, len(fnames))
+	for i, fname := range fnames {
+		var fname2 string
+		fname2, err = ffmpeg.ToMono(fname)
+		defer os.Remove(fname2)
+		if err != nil {
+			return
+		}
+		_, fnames2[i] = filepath.Split(fname2)
+		sampleEnd[i], _, err = ffmpeg.NumSamples(fname2)
+		if err != nil {
+			return
+		}
+		if i > 0 {
+			sampleEnd[i] = sampleEnd[i] + sampleEnd[i-1]
+		}
+		if sampleEnd[i] > 44100*12 {
+			sampleEnd[i] = 44100 * 12
+		}
+		sampleEnd[i] = sampleEnd[i]
+		log.Debugf("%s end: %d", fname, sampleEnd[i])
+	}
+	f.Close()
+
+	log.Debug(fnames)
+	fname2, err := ffmpeg.Concatenate(fnames2)
+	defer os.Remove(fname2)
+	if err != nil {
+		return
+	}
+
+	drumPatch := op1.NewDrumPatch()
+	for i, _ := range drumPatch.Start {
+		if i == len(sampleEnd) {
+			break
+		}
+		if i == 0 {
+			drumPatch.Start[i] = 0
+		} else {
+			drumPatch.Start[i] = (sampleEnd[i-1]) * op1.SAMPLECONVERSION
+		}
+		drumPatch.End[i] = (sampleEnd[i]) * op1.SAMPLECONVERSION
+	}
+
+	err = drumPatch.Save(fname2, finalName)
+	if err == nil {
+		fmt.Printf("converted %+v -> %s\n", fnames, finalName)
+	}
+	return
+}
